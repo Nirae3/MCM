4bdd94f7685af0315fd43df90e065b232c2d6ca5
Nirae3 <ardiarkel3@gmail.com>
2025-01-26 16:43:44 -0500

    won't touch Hotel first time interaction anymore

--- Mesa/tourism_model.py
+++ Mesa/tourism_model.py
@@ -7,23 +7,41 @@ class Tourist(mesa.Agent):
         self.current_location = current_location
 
     def interact_with_hotel(self, hotel):
-        hotel_room_price=hotel.get_hotel_room_cost()
-        hotel_service_price=hotel.get_hotel_service_cost()
-        hotel_room_price=random.randint(100,500)
-        hotel_service_price=random.randint(0,200)
-        return (f"In total I payed {str(hotel_room_price+hotel_service_price)} $.")
+        hotel_room_price=hotel.get_hotel_room_cost()   # get hotel room price
+        hotel_service_price=hotel.get_hotel_service_cost()   # get service room price
+        hotel_room_price=random.randint(100,500)   # hetel room price range 100-500
+        hotel_service_price=random.randint(0,200)   # hotel service price range 0-200
+
+        hotel_total_price=hotel_room_price+hotel_service_price  # get total hotel price
+        
+
+        hotel_customer_list= hotel.get_customer_list().append(self.unique_id) # get customer line in a string
+
+
+        
+        
+        return (f"In total I payed {str(hotel_total_price)} $.")   # return hotel total price
 
     def step(self):
-        hotel = next(agent for agent in self.model.schedule.agents if isinstance(agent, Hotel))
+        hotel = next(agent for agent in self.model.schedule.agents if isinstance(agent, Hotel)) #define hotel
         print (f"hi my unique id {str(self.unique_id)}. Here is a try to interact with hotel {self.interact_with_hotel(hotel)}")
-
+        print(f"the hotel has {hotel.get_customer_list()} cusotmers")
 
 
 class Hotel(mesa.Agent):
-    def __init__(self, unique_id, model, hotel_room_cost, hotel_service_cost):
+    def __init__(self, unique_id, model, hotel_room_cost, hotel_service_cost, customers):
         super().__init__(unique_id, model)
         self.hotel_room_cost=hotel_room_cost
         self.hotel_service_cost=hotel_service_cost
+        self.customers=[]
+    
+    
+    def get_customer_list(self):
+        return self.customers
+    
+    def get_customer_amount(self):
+        return (len(self.customers))
+
 
 
     def get_hotel_room_cost(self):
@@ -33,6 +51,7 @@ class Hotel(mesa.Agent):
         return self.hotel_service_cost
 
 
+
 class HotelModel(mesa.Model):
     def __init__(self, num_tourists, num_hotels):
         self.num_tourists=num_tourists
@@ -40,12 +59,13 @@ class HotelModel(mesa.Model):
         self.schedule=mesa.time.RandomActivation(self) # self schedule is responseible for the order of agents activation
         
         for i in range (self.num_hotels):
-            hotel=Hotel(i, self, hotel_room_cost=200, hotel_service_cost=50)
+            hotel=Hotel(i, self, hotel_room_cost=200, hotel_service_cost=50, customers=0)
             self.schedule.add(hotel)
 
         for i in range(self.num_tourists):
             tourist = Tourist(i, self, current_location=2)
             self.schedule.add(tourist)
-   
+
     def step(self):
         self.schedule.step()
+   
